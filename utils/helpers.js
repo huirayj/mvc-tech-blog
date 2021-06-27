0c53b1549ca819b3c3505425d782f8f1bab68e2d
Justin Hui <huirayj@gmail.com>
2021-06-27 08:41:09 -0400

    updated util functions

---
+++ utils/helpers.js
@@ -0,0 +1,12 @@
+module.exports = {
+    //formats date to make it more readable
+    format_date: date => {
+        return `${new Date(date).getMonth() + 1}/
+                ${new Date(date).getDate()}/
+                ${new Date(date).getFullYear()}`;
+    },
+    //formats words to plural if there is more than 1 of that word
+    format_plural: (word, amount) => {
+        return (amount !== 1) ? `${word}s` : word;
+    }
+}
