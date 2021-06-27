c523863cf64181c3d632b7507b9ae35fc4360386
Justin Hui <huirayj@gmail.com>
2021-06-27 11:15:41 -0400

    completed routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./api/home-routes.js');
+const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 
-router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
-module.exports;
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
